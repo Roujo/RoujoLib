9d7c67e9b469cc325578f13cad5be000fa62ebf7
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2012-03-03 15:30:14 -0500

    Added PersistenceEngine.

---
+++ src/roujo/lib/io/file/PersistenceEngine.java
@@ -0,0 +1,34 @@
+package roujo.lib.io.file;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class PersistenceEngine {
+	private static final PersistenceEngine instance = new PersistenceEngine();
+
+	public static PersistenceEngine getInstance() {
+		return instance;
+	}
+
+	private PersistenceEngine() {
+	}
+
+	public void save(Object object, String filename) throws IOException {
+		ObjectOutputStream out;
+		out = new ObjectOutputStream(new FileOutputStream(filename));
+		out.writeObject(object);
+		out.close();
+	}
+
+	public Object load(String filename) throws IOException,
+			ClassNotFoundException {
+		ObjectInputStream in;
+		Object object;
+		in = new ObjectInputStream(new FileInputStream(filename));
+		object = in.readObject();
+		return object;
+	}
+}
